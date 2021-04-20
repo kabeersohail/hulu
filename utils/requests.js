2fb69464419d4d7de4c71fbe6ac8a2b23274947c
kabeerSohail7 <kabeersohel@gmail.com>
2021-04-21 03:14:49 +0530

    Completed

--- utils/requests.js
+++ utils/requests.js
@@ -12,35 +12,35 @@ export default{
     },
     fetchAction : {
         title : "Action",
-        url : `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+        url : `/discover/movie?api_key=${API_KEY}&with_genres=28`,
     },
     fetchComedy : {
         title : "Comedy",
-        url : `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+        url : `/discover/movie?api_key=${API_KEY}&with_genres=35`,
     },
     fetchHorror : {
         title : "Horror",
-        url : `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+        url : `/discover/movie?api_key=${API_KEY}&with_genres=27`,
     },
     fetchMystery : {
         title : "Mystery",
-        url : `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+        url : `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
     },
     fetchSciFi : {
         title : "Sci-Fi",
-        url : `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+        url : `/discover/movie?api_key=${API_KEY}&with_genres=878`,
     },
     fetchWestern : {
         title : "Western",
-        url : `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+        url : `/discover/movie?api_key=${API_KEY}&with_genres=37`,
     },
     fetchAnimation : {
         title : "Animation",
-        url : `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+        url : `/discover/movie?api_key=${API_KEY}&with_genres=16`,
     },
     fetchTv : {
         title : "TV Movie",
-        url : `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+        url : `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
     },
   
 }
